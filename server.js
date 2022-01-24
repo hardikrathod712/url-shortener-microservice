2e6c364e8022c3b8381cd9470aa3d151fd99eb4e
Hardik <hardikrathod712.hr@gmail.com>
2022-01-24 12:12:40 +0530

    minor bug

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { nanoid } = require('nanoid')
 const mongoose = require('mongoose')
 const app = express();
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
+mongoose.connect(process.env['MONGO_URI'], { useNewUrlParser: true })
 
 const urlSchema = new mongoose.Schema({
   originalURL: String,
@@ -31,41 +31,55 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+function validURL(str) {
+  var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+    '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+  return !!pattern.test(str);
+}
+
 app.post('/api/shorturl', async (req, res) => {
   let url
   try {
     url = new URL(req.body.url)
-    dns.lookup(url.hostname, (err, address, family) => {
-      if (err) {
-        res.json({ error: 'Invalid Hostname' })
-      } else {
-        console.log(address)
-        URLModel.findOne({ originalURL: url }, (err, data) => {
-          if (err) {
-            res.json({ error: 'connection error' })
-          }
-          if (!data) {
-            const newURL = new URLModel({
-              originalURL: url,
-              shortURL: nanoid()
-            })
-            newURL.save((err, data) => {
-              if (err) res.json({ error: 'error saving' })
+    if (validURL(url)) {
+      dns.lookup(url.hostname, (err, address, family) => {
+        if (err) {
+          res.json({ error: 'Invalid Hostname' })
+        } else {
+          console.log(address)
+          URLModel.findOne({ originalURL: url }, (err, data) => {
+            if (err) {
+              res.json({ error: 'connection error' })
+            }
+            if (!data) {
+              const newURL = new URLModel({
+                originalURL: url,
+                shortURL: nanoid()
+              })
+              newURL.save((err, data) => {
+                if (err) res.json({ error: 'error saving' })
+                res.json({
+                  original_url: data.originalURL,
+                  short_url: data.shortURL
+                })
+              })
+            }
+            else {
               res.json({
                 original_url: data.originalURL,
                 short_url: data.shortURL
               })
-            })
-          }
-          else {
-            res.json({
-              original_url: data.originalURL,
-              short_url: data.shortURL
-            })
-          }
-        })
-      }
-    })
+            }
+          })
+        }
+      })
+    } else {
+      res.json({ error: 'invalid url' })
+    }
   } catch (err) {
     res.json({
       error: 'invalid url'
